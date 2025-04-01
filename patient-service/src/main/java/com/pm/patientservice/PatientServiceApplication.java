65eb967061a6a58d141a201095d9be7a88c766fe
iam-ark <abdulrahmankhan021@gmail.com>
2025-04-01 17:47:50 +0530

    1:41:42

---
+++ patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
@@ -0,0 +1,21 @@
+package com.pm.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class PatientServiceApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientServiceApplication.class, args);
+    }
+
+}
+
+
